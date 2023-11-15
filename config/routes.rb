6d5666fe00fd5d31995b5d0283e9828f484f3cec
jeremy madaule <jeremy.madaule@gmail.com>
2023-11-15 23:30:52 +0100

    Exo 2 - Refactoré avec le formulaire dans la page Restaurant/Show

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :restaurants do
-    resources :reviews, only: %i[new create index]
+    resources :reviews, only: %i[create]
   end
 
   # Defines the root path route ("/")
